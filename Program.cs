edb589d1ab7646de5275807565e6b76d6347ca6b
Farhad Shariat <farhad.shariat3@gmail.com>
2024-04-18 10:48:58 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Vio;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("farhad, farhad!");
+    }
+}
